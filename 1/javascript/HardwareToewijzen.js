cace889b66c16ac7c268938f1c2d1f67c1ab2cc3
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-11 15:49:30 +0100

    11-2-2026

---
+++ 1/javascript/HardwareToewijzen.js
@@ -0,0 +1,37 @@
+function Naamingevuld() {
+    var naam = document.getElementById("naam").value;
+    var serienummerInput = document.getElementById("serienummer");
+    if (naam != "") {
+        serienummerInput.setAttribute("list", "serienummerLijst");
+    }
+    else {
+        serienummerInput.removeAttribute("list");
+    }
+}
+
+function naamzoeken() {
+  var naamInput, serienummerInput, naamFilter, serienummerFilter, table, tr, td, i, naamValue, serienummerValue;
+  naamInput = document.getElementById("naam");
+  serienummerInput = document.getElementById("serienummer");
+  naamFilter = naamInput.value.toUpperCase();
+  serienummerFilter = serienummerInput.value.toUpperCase();
+  table = document.getElementById("Toegewezen");
+  tr = table.getElementsByTagName("tr");
+  
+  for (i = 1; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td");
+    if (td.length > 1) {
+      naamValue = (td[0].textContent || td[0].innerText).toUpperCase();
+      serienummerValue = (td[1].textContent || td[1].innerText).toUpperCase();
+      
+      var naamMatch = naamFilter === "" || naamValue.indexOf(naamFilter) > -1;
+      var serienummerMatch = serienummerFilter === "" || serienummerValue.indexOf(serienummerFilter) > -1;
+      
+      if (naamMatch && serienummerMatch) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
+  }
+}
--- 1/javascript/Verantwoordelijke.js
+++ 1/javascript/Verantwoordelijke.js
@@ -1,3 +1,5 @@
-function VerwijderProduct() {
-    confirm("Weet je zeker dat je het product wilt verwijderen?");
+function VerwijderProduct(product) {
+    confirm("Weet je zeker dat je " + product + " wilt verwijderen? \nDeze actie kan niet ongedaan worden gemaakt.");
+    // Hier kun je de logica toevoegen om het product daadwerkelijk te verwijderen
+    console.log("Product verwijderd: " + [product]);
 }
