3fa0df6c08fce3926235b0b5dbc87b1830063bd0
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-12 14:39:49 +0100

    12-2-2026 14:36 Werkt php?

--- backend/config.php
+++ backend/config.php
@@ -1,4 +1,6 @@
 <?php
+echo "PHP Werkt";
+
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include 'backend/config.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
