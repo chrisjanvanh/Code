8c4c7d1e32a20d83b86d1710fd90a2615de274c6
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-17 15:49:57 +0100

    17-2-2026 15:49

--- HR.php
+++ HR.php
@@ -1,3 +1,14 @@
+<?php
+require 'backend/config.php';
+
+$afdeling = "HR"; // deze pagina is voor HR
+
+// Haal alle producten op waarvoor HR verantwoordelijk is
+$stmt = $conn->prepare("SELECT * FROM Product WHERE Afdeling = ?");
+$stmt->bind_param("s", $afdeling);
+$stmt->execute();
+$producten = $stmt->get_result();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,54 +20,93 @@
     <script src="javascript/auth.js"></script>
     <script src="javascript/HR.js"></script>
     <script src="javascript/main.js"></script>
-
-    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
 </head>
 <body>
-    <header>
-        <img src="img/menu.png" alt="Menu button" class="menu-button" set onclick="toggleMenu()">
-        <img src="img/logo.svg" alt="VDL Groep Logo">
-        <a href="index.php">Homepagina</a>
-        <a href="HuidigeToegangen.php">Medewerkers</a>
-        <a href="HardwareToewijzen.php"> Hardware</a>
-        <a href="Verantwoordelijke.php">Producten</a>
-        <a href="HR.php" class="No-Mobile" class="current">HR</a>
-    </header>
+<header>
+    <img src="img/menu.png" alt="Menu button" class="menu-button" set onclick="toggleMenu()">
+    <img src="img/logo.svg" alt="VDL Groep Logo">
+    <a href="index.php">Homepagina</a>
+    <a href="HuidigeToegangen.php">Medewerkers</a>
+    <a href="HardwareToewijzen.php"> Hardware</a>
+    <a href="Verantwoordelijke.php">Producten</a>
+    <a href="HR.php" class="current">HR</a>
+</header>
 
-    <div class="content">
-        <h1>Taken</h1>
-        <table>
+<div class="content">
+    <h1>Taken</h1>
+
+    <table>
+        <tr>
+            <th>Product</th>
+            <th>Medewerker</th>
+            <th>Functie</th>
+            <th>Locatie</th>
+            <th>Leidinggevende</th>
+            <th>Bedrijf</th>
+            <th>Referentie</th>
+            <th>Taak</th>
+        </tr>
+
+        <?php while ($product = $producten->fetch_assoc()): ?>
+
+            <?php
+            // kolomnaam in Medewerker-tabel
+            $kolom = $product['Product'];
+
+            // Haal medewerkers op die 0 of 2 hebben
+            $sql = "SELECT * FROM Medewerker WHERE `$kolom` IN (0,2)";
+            $medewerkers = $conn->query($sql);
+            ?>
+
+            <?php while ($m = $medewerkers->fetch_assoc()): ?>
                 <tr>
-                <th>Product</th>
-                <th>Medewerker</th>
-                <th>Functie</th>
-                <th>Locatie</th>
-                <th>Leidinggevende</th>
-                <th>Bedrijf</th>
-                <th>Referentie</th>
-                <th>Taak afronden</th>
-            </tr>
+                    <td><?= $product['Product'] ?></td>
+                    <td><?= $m['Naam'] ?></td>
+                    <td><?= $m['Functie'] ?></td>
+                    <td><?= $m['Locatie'] ?></td>
+                    <td><?= $m['Leidinggevende'] ?></td>
+                    <td><?= $m['Bedrijf'] ?></td>
+                    <td><?= $m['Referentie'] ?></td>
+
+                    <td>
+                        <?php if ($m[$kolom] == 0): ?>
+                            <button class="button" onclick="afronden(<?= $m['ID'] ?>, '<?= $kolom ?>', 1)">Toevoegen afronden</button>
+                        <?php elseif ($m[$kolom] == 2): ?>
+                            <button class="button" onclick="afronden(<?= $m['ID'] ?>, '<?= $kolom ?>', 1)">Verwijderen afronden</button>
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+
+        <?php endwhile; ?>
+    </table>
+
+    <h2>Mailadressen voor deze afdeling</h2>
+
+    <?php
+    $emails = $conn->query("SELECT * FROM AfdelingEmails WHERE Afdeling = '$afdeling'");
+    ?>
+
+    <table>
+        <tr>
+            <th>Email</th>
+            <th>Actie</th>
+        </tr>
+
+        <?php while ($e = $emails->fetch_assoc()): ?>
             <tr>
-                <td>MyVDL</td>
-                <td>Chrisjan van Houtert</td>
-                <td>Stagair</td>
-                <td>Valkenswaard</td>
-                <td>Pim Verlinden</td>
-                <td>VDL Bus &amp; Coach</td>
-                <td>Voorganger</td>
-                <td><button class="button" onclick="confirmAfronden()">Afronden</button></td>
+                <td><?= $e['Email'] ?></td>
+                <td><button onclick="verwijderEmail(<?= $e['ID'] ?>)" class="button">Verwijder</button></td>
             </tr>
-            <tr>
-                <td>MyVDL</td>
-                <td>Pim Verlinden</td>
-                <td>Manager Business IT</td>
-                <td>Valkenswaard</td>
-                <td>Dennis van Opzeeland</td>
-                <td>VDL Bus &amp; Coach</td>
-                <td>Voorganger</td>
-                <td><button class="button" onclick="confirmAfronden()">Afronden</button></td>
-            </tr>
-        </table>
-    </div>
+        <?php endwhile; ?>
+
+        <tr>
+            <td colspan="2">
+                <button onclick="voegEmailToe()" class="button">Email toevoegen</button>
+            </td>
+        </tr>
+    </table>
+
+</div>
 </body>
 </html>
