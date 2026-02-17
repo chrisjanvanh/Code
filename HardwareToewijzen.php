fed8a3c2f91792d203a4fb9852d7bf2fd94b857d
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-17 10:18:43 +0100

    17-2-2026 10:18

--- HardwareToewijzen.php
+++ HardwareToewijzen.php
@@ -1,3 +1,65 @@
+<?php
+require 'backend/config.php';
+
+$melding = "";
+
+// Ophalen van alle toegewezen hardware
+$result = $conn->query("SELECT * FROM Gebruiksname ORDER BY Naam ASC");
+$toegewezen = $result->fetch_all(MYSQLI_ASSOC);
+
+// Opslaan van nieuwe toewijzing
+if (isset($_POST['opslaan'])) {
+
+    $naam = $_POST['naam'];
+    $serienummer = $_POST['serienummer'];
+    $uitgiftedatum = $_POST['uitgiftedatum'];
+
+    // Check of medewerker bestaat
+    $checkNaam = $conn->prepare("SELECT 1 FROM Medewerker WHERE Naam = ?");
+    $checkNaam->bind_param("s", $naam);
+    $checkNaam->execute();
+    $checkNaam->store_result();
+
+    if ($checkNaam->num_rows == 0) {
+        $melding = "Deze medewerker bestaat niet.";
+    } else {
+
+        // Check of serienummer al bestaat
+        $checkSN = $conn->prepare("SELECT 1 FROM Gebruiksname WHERE Serienummer = ?");
+        $checkSN->bind_param("s", $serienummer);
+        $checkSN->execute();
+        $checkSN->store_result();
+
+        if ($checkSN->num_rows > 0) {
+            $melding = "Dit serienummer is al toegewezen.";
+        } else {
+
+            // INSERT uitvoeren
+            $stmt = $conn->prepare("INSERT INTO Gebruiksname (Serienummer, Naam, Uitgiftedatum) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss", $serienummer, $naam, $uitgiftedatum);
+
+            if ($stmt->execute()) {
+                $melding = "Hardware succesvol toegewezen!";
+                header("Refresh:0"); // pagina herladen zodat tabel update
+            } else {
+                $melding = "Er is iets fout gegaan.";
+            }
+        }
+    }
+}
+
+// Verwijderen
+if (isset($_POST['verwijder'])) {
+    $sn = $_POST['verwijder'];
+
+    $del = $conn->prepare("DELETE FROM Gebruiksname WHERE Serienummer = ?");
+    $del->bind_param("s", $sn);
+    $del->execute();
+
+    header("Refresh:0");
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -24,55 +86,42 @@
 
     <div class="content">
         <h1>Hardware Toewijzen</h1>
-        <form action="">
+        <form method="POST">
             Naam: <br>
             <input onchange="Naamingevuld()" type="text" id="naam" name="naam"><br>
+
             Serienummer: <br>
             <input type="text" id="serienummer" name="serienummer"><br>
-            <datalist id="serienummerLijst">
-                <option value="BCO11L-3171M2L"></option>
-                <option value="BCO011L-3171M2C"></option>
-                <option value="BCO011L-3171M2V"></option>
-                <option value="BCO011L-3171M2T"></option>
-            </datalist>
+
             Uitgiftedatum: <br>
             <input type="date" id="uitgiftedatum" name="uitgiftedatum"><br><br>
+
+            <button class="button" name="opslaan">Opslaan</button>
         </form>
-            <button class="button" id="Opslaan">Opslaan</button>
-            <button class="button" id="zoek" onclick="naamzoeken()">Zoek</button>
+
+            <button type="button" class="button" id="zoek" onclick="naamzoeken()">Zoek</button>
             <a href="HardwareToevoegen.php" class="button">Hardware Toevoegen</a>
 <br><br><br>
-        <table id="Toegewezen">
+                <table id="Toegewezen">
             <tr>
                 <th>Naam</th>
                 <th>Serienummer</th>
                 <th>Uitgiftedatum</th>
                 <th>Verwijderen</th>
             </tr>
-            <tr>
-                <td>Chrisjan van Houtert</td>
-                <td>BCO11L-3171M2L</td>
-                <td>09-02-2025</td>
-                <td><button class="button">Verwijderen</button></td>
-            </tr>
-            <tr>
-                <td>Chrisjan van Houtert</td>
-                <td>BCO011L-3171M2C</td>
-                <td>02-02-2026</td>
-                <td><button class="button">Verwijderen</button></td>
-            </tr>
-            <tr>
-                <td>Pim Verlinden</td>
-                <td>BCO011L-3171M2V</td>
-                <td>02-10-2025</td>
-                <td><button class="button">Verwijderen</button></td>
-            </tr>
-            <tr style="color: green;">
-                <td>Voorraad</td>
-                <td>BCO011L-3171M2T</td>
-                <td>11-02-2026</td>
-                <td><button class="button">Verwijderen</button></td>
-            </tr>
+
+            <?php foreach ($toegewezen as $row): ?>
+                <tr>
+                    <td><?= htmlspecialchars($row['Naam']) ?></td>
+                    <td><?= htmlspecialchars($row['Serienummer']) ?></td>
+                    <td><?= htmlspecialchars($row['Uitgiftedatum']) ?></td>
+                    <td>
+                        <form method="POST" onsubmit="return confirm('Weet je zeker dat je dit wilt verwijderen?')">
+                            <button class="button" name="verwijder" value="<?= $row['Serienummer'] ?>">Verwijderen</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
         </table>
     </div>
 </body>
