218eb3b774ab8195c8644f4e186a79110e52a47f
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-17 12:53:20 +0100

    17-2-2026 12:52

--- HardwareToewijzen.php
+++ HardwareToewijzen.php
@@ -33,7 +33,7 @@ if (isset($_POST['opslaan'])) {
         $checkNaam->store_result();
 
         if ($checkNaam->num_rows == 0) {
-            alert =("Deze medewerker bestaat niet. Voeg de medewerker eerst toe voordat je hardware toewijst.");
+            echo "<script>alert('Deze medewerker bestaat niet. Voeg de medewerker eerst toe voordat je hardware toewijst.');</script>";
         } else {
 
             /* 2.2 Check of serienummer bestaat in Hardware */
