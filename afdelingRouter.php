e843c42b4b0495714506034fa70f1d8efae2e1c4
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-18 13:39:14 +0100

    18-2-2026 13:39

--- afdelingRouter.php
+++ afdelingRouter.php
@@ -42,7 +42,7 @@ if (count($afdelingen) === 1) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VDL Bus & Coach</title>
     <link rel="stylesheet" href="css/style.css">
-    <!-- <link rel="stylesheet" href="css/afdelingRouter.css"> -->
+    <link rel="stylesheet" href="css/afdelingRouter.css">
 
 </head>
 <body>
