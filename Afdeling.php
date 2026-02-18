625a389fc2880a6b5e4a13281cc616e96f8a23db
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-18 11:10:26 +0100

    18-2-2026 11:10

--- Afdeling.php
+++ Afdeling.php
@@ -16,9 +16,9 @@ $producten = $stmt->get_result();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stage</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/HR.css">
+    <link rel="stylesheet" href="css/Afdeling.css">
     <script src="javascript/auth.js"></script>
-    <script src="javascript/HR.js"></script>
+    <script src="javascript/Afdeling.js"></script>
     <script src="javascript/main.js"></script>
 
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
