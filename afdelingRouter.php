9374d1afb3eee9e1f32cc53dce6eb979bac8317b
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-18 13:42:10 +0100

    18-2-2026 13:42

--- afdelingRouter.php
+++ afdelingRouter.php
@@ -49,11 +49,11 @@ if (count($afdelingen) === 1) {
     <header>
         <img src="img/menu.png" alt="Menu button" class="menu-button" set onclick="toggleMenu()">
         <img src="img/logo.svg" alt="VDL Groep Logo">
-        <a href="index.php" class="current">Homepagina</a>
+        <a href="index.php">Homepagina</a>
         <a href="HuidigeToegangen.php">Medewerkers</a>
         <a href="HardwareToewijzen.php"> Hardware</a>
         <a href="Verantwoordelijke.php">Producten</a>
-        <a href="afdelingRouter.php" class="no-mobile">HR</a>
+        <a href="afdelingRouter.php" class="current">HR</a>
         <div style="
             position: absolute;
             right: 20px;
