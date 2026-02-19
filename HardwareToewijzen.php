571c3153337324a2bca1f3874b3df21e47a16372
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-19 15:51:27 +0100

    19-2-2026 15:51

--- HardwareToewijzen.php
+++ HardwareToewijzen.php
@@ -82,7 +82,9 @@
             <tr>
                 <td><?= htmlspecialchars($row['Naam']) ?></td>
                 <td><?= htmlspecialchars($row['Serienummer']) ?></td>
-                <td><?= date("d-m-Y", strtotime($row['Uitgiftedatum'])) ?></td>
+                <td>
+                    <?= $row['Uitgiftedatum'] ? date("d-m-Y", strtotime($row['Uitgiftedatum'])) : '-' ?>
+                </td>
                 <td>
                     <form method="POST" onsubmit="return confirm('Weet je zeker dat je dit wilt verwijderen?')">
                         <button class="button" name="verwijder" value="<?= $row['Serienummer'] ?>">Verwijderen</button>
