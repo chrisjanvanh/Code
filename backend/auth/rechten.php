2eb587987b95d9edffcd3bd0f56fb08f68b898fe
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-26 11:34:28 +0100

    26-2-2026 11:34

--- backend/auth/rechten.php
+++ backend/auth/rechten.php
@@ -47,10 +47,11 @@ function toonMenu($rechten, $current = "")
             <a href="afdelingRouter.php" class="<?= $current === 'taken' ? 'current' : '' ?>">Taken</a>
         <?php endif; ?>
 
-        <div style="position: absolute; right: 15px; display: flex; align-items: center;">
-            <a href="logout.php"
-               style="padding: 8px 15px; background: #e74c3c; color: white;
-                      border-radius: 5px; text-decoration: none;">
+        <div class="logout-container"> 
+        <!-- style="position: absolute; right: 15px; display: flex; align-items: center;"> -->
+            <a href="logout.php" class="logout-button">
+               <!-- style="padding: 8px 15px; background: #e74c3c; color: white;
+                      border-radius: 5px; text-decoration: none;" -->
                 Uitloggen
             </a>
         </div>
