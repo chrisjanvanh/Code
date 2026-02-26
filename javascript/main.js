325682af385c4f22e62a98fdacdc9dd69eab21a0
chrisjanvanh <chrisjanvanh@gmail.com>
2026-02-26 12:54:43 +0100

    26-2-2026 12:54

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,16 @@
+var lastScrollTop = 0;
+
+$(window).scroll(function () {
+  
+var st = $(this).scrollTop();
+        if (st < lastScrollTop){
+            $('header').slideDown();
+        } else {
+          $('header').slideUp();
+        }
+        lastScrollTop = st;
+  })
+
 function toggleMenu() {
     const header = document.querySelector('header');
     header.classList.toggle('open');
