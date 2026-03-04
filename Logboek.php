9addefe68d97dc623f649b425a749da0f86db26c
chrisjanvanh <chrisjanvanh@gmail.com>
2026-03-04 09:04:52 +0100

    4-3-2026 9:04

--- Logboek.php
+++ Logboek.php
@@ -15,7 +15,7 @@ require "backend/logboek.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VDL Bus & Coach</title>
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/Logboek.css">
     <script src="javascript/main.js"></script>
 
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
