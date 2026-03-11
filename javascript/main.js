f63289a9036321d118815792267573dbdba93e8b
chrisjanvanh <chrisjanvanh@gmail.com>
2026-03-11 12:19:23 +0100

    11-3-2026 12:19

--- javascript/main.js
+++ javascript/main.js
@@ -5,8 +5,10 @@ window.addEventListener("scroll", function () {
 
     if (st > lastScrollTop) {
         document.querySelector("header").classList.add("hide");   // scroll omlaag → verbergen
+        document.querySelector("content").classList.add("hide");   // scroll omlaag → verbergen
     } else {
         document.querySelector("header").classList.remove("hide"); // scroll omhoog → tonen
+        document.querySelector("content").classList.remove("hide");   // scroll omlaag → verbergen
     }
 
     lastScrollTop = st <= 0 ? 0 : st;
