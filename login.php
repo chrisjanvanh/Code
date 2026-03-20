5d79a71e7d1e533910b5d027504f1496a9e7cb71
chrisjanvanh <chrisjanvanh@gmail.com>
2026-03-20 11:06:50 +0100

    20-3-2026 11:06

--- login.php
+++ login.php
@@ -53,7 +53,7 @@ if (isset($_SESSION['email'])) {
         img {
             max-width: 150px;
             margin-top: 5px;
-            transition: max-width 1.0s ease;
+            transition: all 1.0s ease;
         }
         img:hover {
             max-width: 160px;
