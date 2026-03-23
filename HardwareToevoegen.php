0fcc298f6d5e995ae4b87f5990781767073f7eff
chrisjanvanh <chrisjanvanh@gmail.com>
2026-03-23 10:48:31 +0100

    23-3-2026 10:48

--- HardwareToevoegen.php
+++ HardwareToevoegen.php
@@ -23,44 +23,44 @@ require_once 'backend/auth/rechten.php';
 toonMenu($rechten, 'hardware');
 ?>
 
-<div class="content">
+    <div class="content">
 
-    <?php if (!empty($_SESSION['melding'])): ?>
-        <div class="melding"><?= $_SESSION['melding'] ?></div>
-        <?php unset($_SESSION['melding']); ?>
-    <?php endif; ?>
+        <?php if (!empty($_SESSION['melding'])): ?>
+            <div class="melding"><?= $_SESSION['melding'] ?></div>
+            <?php unset($_SESSION['melding']); ?>
+        <?php endif; ?>
 
-    <form method="POST">
-        <h2>Hardware toevoegen</h2>
+        <form method="POST">
+            <h2>Hardware toevoegen</h2>
 
-        Serienummer:<br>
-        <input type="text" name="serienummer" required>
+            Serienummer:<br>
+            <input type="text" name="serienummer" required>
 
-        Bedrijf:
-        <input list="bedrijven" id="bedrijf" name="bedrijf" required>
+            Bedrijf:
+            <input list="bedrijven" id="bedrijf" name="bedrijf" required>
 
-        <datalist id="bedrijven">
-            <?php foreach ($bedrijven as $b): ?>
-                <option value="<?= htmlspecialchars($b) ?>"></option>
-            <?php endforeach; ?>
-        </datalist>
+            <datalist id="bedrijven">
+                <?php foreach ($bedrijven as $b): ?>
+                    <option value="<?= htmlspecialchars($b) ?>"></option>
+                <?php endforeach; ?>
+            </datalist>
 
-        Merk:<br>
-        <input type="text" name="merk" >
+            Merk:<br>
+            <input type="text" name="merk" >
 
-        Model:<br>
-        <input type="text" name="model">
+            Model:<br>
+            <input type="text" name="model">
 
-        Prijs:<br>
-        <input type="text" name="prijs">
+            Prijs:<br>
+            <input type="text" name="prijs">
 
-        Aankoopdatum:<br>
-        <input type="date" name="aankoopdatum">
+            Aankoopdatum:<br>
+            <input type="date" name="aankoopdatum">
 
-        <input type="submit" value="Opslaan">
-        <a href="HardwareBestellen.php" class="button">Hardware Bestellen</a>
-    </form>
+            <input type="submit" value="Opslaan">
+            <a href="HardwareBestellen.php" class="button">Hardware Bestellen</a>
+        </form>
 
-</div>
+    </div>
 </body>
 </html>
