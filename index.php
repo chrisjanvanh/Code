269ec8b9c42a6954a86556d0c7d5b3d437188266
chrisjanvanh <chrisjanvanh@gmail.com>
2026-04-01 11:37:21 +0200

    1-4-2026 11:37

--- index.php
+++ index.php
@@ -11,7 +11,6 @@ if (!isset($_SESSION['email'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="view-transition" content="same-origin">
     <title>VDL Bus & Coach</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
