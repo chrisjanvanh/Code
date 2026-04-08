aa9d488d34c38f3b1f9d78d8906394c54b978f80
chrisjanvanh <chrisjanvanh@gmail.com>
2026-04-08 14:10:23 +0200

    8-4-2026 14:10

--- HardwareToewijzen.php
+++ HardwareToewijzen.php
@@ -1,5 +1,11 @@
 <?php 
 require 'backend/hardware_toewijzen_logic.php'; 
+
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("Location: login.php");
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
