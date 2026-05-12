c31376ed85f586f08fab6efcf33d9388992a3b3c
chrisjanvanh <chrisjanvanh@gmail.com>
2026-05-12 09:02:36 +0200

    12-5-2026 9:02

--- backend/setSession.php
+++ backend/setSession.php
@@ -1,5 +1,5 @@
 <?php
-require "config2.php";
+// require "config2.php";
 
 header("Cross-Origin-Opener-Policy: unsafe-none");
 header("Cross-Origin-Embedder-Policy: unsafe-none");
@@ -18,8 +18,8 @@ $_SESSION['gebruikernaam'] = $_POST['gebruikernaam'];
 $email = $_POST['email'] ?? '';
 $gebruikernaam = $_POST['gebruikernaam'] ?? '';
 
-$stmt = $pdo->prepare("INSERT INTO Logboek (Actie, Soort) VALUES (?, ?)");
-$stmt->execute(["$gebruikernaam is ingelogd met het E-mailadres $email", "Inloggen"]);
+// $stmt = $pdo->prepare("INSERT INTO Logboek (Actie, Soort) VALUES (?, ?)");
+// $stmt->execute(["$gebruikernaam is ingelogd met het E-mailadres $email", "Inloggen"]);
 
 echo "OK";
 ?>
