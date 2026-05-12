25dfa5e4b5d846ac9e1531bc2efaee86aa643ad2
chrisjanvanh <chrisjanvanh@gmail.com>
2026-05-12 08:43:02 +0200

    12-5-2026 8:42

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 header("Cross-Origin-Opener-Policy: unsafe-none");
 header("Cross-Origin-Embedder-Policy: unsafe-none");
+header("Permissions-Policy: identity-credentials-get=*");
 
 session_start();
 
@@ -24,6 +25,7 @@ if (isset($_SESSION['email'])) {
     <script src="https://alcdn.msauth.net/browser/2.37.0/js/msal-browser.min.js"></script>
 
     <!-- Google SSO -->
+    <meta name="google-signin-client_id" content="208183931140-rafgpe00evlhagalk0adasd7ffelt5is.apps.googleusercontent.com">
     <script src="https://accounts.google.com/gsi/client" async defer></script>
 
     <style>
@@ -67,7 +69,7 @@ if (isset($_SESSION['email'])) {
 </head>
 
 <body>
-    <main>
+<main>
 <div class="login-container">
     <div class="login-box">
         <img src="img/logo.svg" alt="VDL Groep Logo">
@@ -108,7 +110,7 @@ const msalConfig = {
     auth: {
         clientId: "00fc974d-f877-4135-8bb4-fb6345b5bac4",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: window.location.href
+        redirectUri: "https://vdl-backend.azurewebsites.net/auth/callback.html"
     }
 };
 
