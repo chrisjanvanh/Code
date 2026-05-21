00db73f17674dfce0a2a84a5c1e12f9d3dfee0b4
chrisjanvanh <chrisjanvanh@gmail.com>
2026-05-21 14:35:15 +0200

    21-5-2026 14:35

--- backend/hardware_toewijzen_logic.php
+++ backend/hardware_toewijzen_logic.php
@@ -1,12 +1,12 @@
 <?php
-session_set_cookie_params([
-    'lifetime' => 0,
-    'path' => '/',
-    'domain' => '',
-    'secure' => true,
-    'httponly' => true,
-    'samesite' => 'None'
-]);
+// session_set_cookie_params([
+//     'lifetime' => 0,
+//     'path' => '/',
+//     'domain' => '',
+//     'secure' => true,
+//     'httponly' => true,
+//     'samesite' => 'None'
+// ]);
 
 session_start();
 
