b58f1e687d5a6c7fff4245a289d3011b7a66b57a
chrisjanvanh <chrisjanvanh@gmail.com>
2026-06-02 15:20:40 +0200

    2-6-2026 15:20

--- HuidigeToegangen.php
+++ HuidigeToegangen.php
@@ -89,6 +89,11 @@ toonMenu($rechten, 'medewerkers');
             Email:<br>
             <input type="email" name="email" value="<?= $medewerker['Email'] ?>" autocomplete="off"><br>
             </label>
+            
+            <label>
+            Startdatum:<br>
+            <input type="date" name="startdatum" value="<?= $medewerker['Startdatum'] ?>"><br>
+            </label>
 
             <input type="hidden" name="naam" value="<?= $medewerker['Naam'] ?>">
 
