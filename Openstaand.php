75b90c92fad76b03f3136250a70d88ac6f980910
chrisjanvanh <chrisjanvanh@gmail.com>
2026-06-03 15:22:18 +0200

    3-6-2026 15:22

--- Openstaand.php
+++ Openstaand.php
@@ -69,7 +69,7 @@ toonMenu($rechten, 'medewerkers');
                     <tr>
                         <td><?= htmlspecialchars($taak['Medewerker']) ?></td>
                         <td><?= htmlspecialchars($taak['Product']) ?></td>
-                        <td><?= htmlspecialchars($placeholderBedrijf) ?></td>
+                        <td><?= htmlspecialchars($placeholdersBedrijven) ?></td>
                         <td>
                             <?php
                             $kleur = match ($taak['Waarde']) {
