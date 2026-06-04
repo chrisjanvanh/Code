1055cd601af841b0dd8c82b717a3ea0218a297e2
chrisjanvanh <chrisjanvanh@gmail.com>
2026-06-04 15:47:47 +0200

    4-6-2026 15:48

--- backend/export_hardware_toewijzen.php
+++ backend/export_hardware_toewijzen.php
@@ -24,7 +24,7 @@ if ($bedrijf === '') {
 }
 
 header("Content-Type: text/csv; charset=utf-8");
-header("Content-Disposition: attachment; filename=hardware_toewijzen_{$bedrijf}_" . date("Ymd_His") . ".csv");
+header("Content-Disposition: attachment; filename=hardware{$bedrijf}_" . date("Y-m-d_H-i-s") . ".csv");
 echo "\xEF\xBB\xBF";
 
 $output = fopen("php://output", "w");
